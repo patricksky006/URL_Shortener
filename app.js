eb3cd80af18679cf73bc4b2551e75b10b233f985
patricksky006 <patrick.sky@hotmail.com>
2023-08-14 21:23:15 +0800

    Bulid: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('express app for URL_Shortener')
+  res.render('index')
 })
 
 app.listen(port, () => {
